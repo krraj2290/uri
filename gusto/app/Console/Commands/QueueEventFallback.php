e572458aafb6861c87d28584a1366918f450fb00
pramodrohuma <pramod@rohuma.com>
2019-01-08 18:26:46 +0530

    event

--- gusto/app/Console/Commands/QueueEventFallback.php
+++ gusto/app/Console/Commands/QueueEventFallback.php
@@ -63,10 +63,14 @@ class QueueEventFallback extends Command {
                         }
                         try {
                             $addArr = array(
-                                'mobapp_id' => $queueDataArr['app_id'], 'fan_id' => $queueDataArr['fan_id'],
-                                'section' => $queueDataArr['channel'], 'event' => $queueDataArr['event'],
-                                'sub_event' => $queueDataArr['sub_event'], 'extra' => $queueDataArr['extra'],
-                                'timestamp' => $queueDataArr['timestamp'], 'transaction_id' => $queueDataArr['guid'],
+                                'mobapp_id' => isset($queueDataArr['app_id']) ? $queueDataArr['app_id'] : 0, 
+                                'fan_id' => isset($queueDataArr['fan_id']) ? $queueDataArr['fan_id'] : 0,
+                                'section' => isset($queueDataArr['channel']) ? $queueDataArr['channel'] : '', 
+                                'event' => isset($queueDataArr['event']) ? $queueDataArr['event'] : '',
+                                'sub_event' => isset($queueDataArr['sub_event']) ? $queueDataArr['sub_event'] : "", 
+                                'extra' => isset($queueDataArr['extra']) ? $queueDataArr['extra'] : "",
+                                'timestamp' => isset($queueDataArr['timestamp']) ? $queueDataArr['timestamp'] : time(),
+                                'transaction_id' => isset($queueDataArr['guid']) ? $queueDataArr['guid'] : "",
                             );
                             // save the entry 
 //                            $objEvents = new EventController();
